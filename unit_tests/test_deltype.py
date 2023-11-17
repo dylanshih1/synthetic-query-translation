6caf6473b6905a18401ce2da54561e1c1ce8e122
iparris1 <iparris1@jhu.edu>
2023-11-17 07:34:35 -0500

    postagger unit tests

--- unit_tests/test_deltype.py
+++ unit_tests/test_deltype.py
@@ -4,9 +4,16 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
 from postagger import POSTagger
 
-def remove_types_test():
-    Tagger = POSTagger(["The quick brown fox.", "Jumps over the lazy dog."])
-    Tagger.remove_types(["PUNCT"])
-    assert Tagger.sentences == ["The quick brown fox", "Jumps over the lazy dog"]
+def test_get_sentences():
+    tagger = POSTagger(["The quick brown fox.", "Jumps over the lazy dog."])
+    assert tagger.sentences == ["The quick brown fox.", "Jumps over the lazy dog."]
 
+def test_remove_types():
+    tagger = POSTagger(["The quick brown fox.", "Jumps over the lazy dog."])
+    tagger.remove_types(["PUNCT"])
+    assert tagger.sentences == ["The quick brown fox", "Jumps over the lazy dog"]
 
+def test_keep_types():
+    tagger = POSTagger(["The quick brown fox.", "Jumps over the lazy dog."])
+    tagger.keep_types(["NOUN", "ADJ"])
+    assert tagger.sentences == ["quick brown fox", "lazy dog"]

1bd970446235d77e106b6ac471fd4c59738c9346
iparris1 <iparris1@jhu.edu>
2023-11-19 15:39:23 -0500

    draft delstop function (basic implementation)

--- src/delstop.py
+++ src/delstop.py
@@ -2,5 +2,14 @@ import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 
+stopwords = set(stopwords.words('english'))
+
 def delete_stopwords(sentences: list[str]) -> list[str]:
-    
+    non_stopwords = []
+    for sentence in sentences:
+        new_sentence = []
+        for word in sentence:
+            if word not in stopwords:
+                new_sentence.append(word)
+        non_stopwords.append(new_sentence)
+    return non_stopwords

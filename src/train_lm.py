2767408e8a4d05e732d721e9e7f7ca8dd88bd0b0
iparris1 <iparris1@jhu.edu>
2023-12-01 09:05:59 -0500

    setting up file

---
+++ src/train_lm.py
@@ -0,0 +1,17 @@
+import os
+import torch
+import torch.nn as nn
+
+class Encoder(nn.Module):
+
+class Decoder(nn.Module):
+
+class Generator(nn.Module):
+
+class EncoderDecoder(nn.Module):
+
+def make_model():
+    return
+
+
+
